6b0cf06e6ca46f1e1a36805a4a722e8721d7f36c
Charles McQuain <cmcquai1@my.westga.edu>
2020-08-15 20:34:03 -0700

    Added props for name and completed to Todo component

--- src/App.js
+++ src/App.js
@@ -42,15 +42,11 @@ function App(props) {
       <h2 id="list-heading">
         3 tasks remaining
       </h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
-      >
-        <Todo />
-        <Todo />
-        <Todo />
-      </ul>
+
+        <Todo name="Eat" completed={true} />
+        <Todo name="Sleep" completed={false} />
+        <Todo name="Repeat" completed={false} />
+
     </div>
   );
 }
